128e784c56ffcf54e35112f9ede19780a8070579
kowshiki-r <kowshikir23@gmail.com>
2025-01-11 13:04:22 +0530

    Create PROBLEM3.c

---
+++ PROBLEM3.c
@@ -0,0 +1,12 @@
+/*3)GET A NUMBER FROM USER AND MULTIPLY 3 TO THAT NUMBER AND PRINT THE RESULT
+INPUT:45 OUTPUT:135 */
+
+#include<stdio.h>
+int main()
+{
+    int num,result;
+    printf("Enter the number");
+    scanf("%d",&num);
+    result=num*3;
+    printf("Result=%d",result);
+}
